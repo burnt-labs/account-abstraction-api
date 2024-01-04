57abea790301415df944eb003e0ae1c32979013c
Francis Roch <francis@manafont.net>
2024-01-03 19:41:10 -0500

    Change deployment to a statefulset

--- manifests/testnet/configmaps/start.sh
+++ manifests/testnet/configmaps/start.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
+
+if [[ $HOSTNAME =~ ([0-9]+)$ ]]; then
+    export ORDINAL=${BASH_REMATCH[1]}
+    echo "***"
+    echo "This is pod number $ORDINAL in the StatefulSet."
+    echo "***"
+else
+    echo "Could not determine the ordinal number."
+fi
+
+export varname="MNEMONIC_$ORDINAL"
+if [ -z "$varname" ]; then
+    echo "Env var $varname is not set."
+else
+    echo "Env var $varname is set."
+fi
 
 #/bin/sleep infinity
 npm run start
